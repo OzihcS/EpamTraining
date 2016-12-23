a210b9980277ff473eead56d270b298a5b829071
Anthony Piontkowski <ant.piontkowski@gmail.com>
2016-12-23 21:22:34 +0200

    Practice8 and SummaryTask3

---
+++ SummaryTask3/src/ua/nure/piontkovskyi/SummaryTask3/Demo.java
@@ -0,0 +1,12 @@
+package ua.nure.piontkovskyi.SummaryTask3;
+
+import ua.nure.piontkovskyi.SummaryTask3.util.MyValidator;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        Main.main(new String[]{"input.xml"});
+        MyValidator.main(new String[]{});
+    }
+
+}
